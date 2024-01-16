770d983415e87642b2b8228acc3147778c5dd824
TruongManhHoang <truongmanhhoang481@gmail.com>
2024-01-16 22:06:50 +0700

    first commit

--- ProjectMVC/obj/Debug/net8.0/NewProject.AssemblyInfo.cs
+++ ProjectMVC/obj/Debug/net8.0/NewProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NewProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb296ca6470769f1f6a461a9794ab0d8ddd54d51")]
 [assembly: System.Reflection.AssemblyProductAttribute("NewProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NewProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
