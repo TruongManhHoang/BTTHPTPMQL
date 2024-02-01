b1dc81f74fd9c454e7ae4c1523c1c2dcfca63daf
TruongManhHoang <127612285+TruongManhHoang@users.noreply.github.com>
2024-02-01 21:35:05 +0700

    Delete ProjectMVC directory

--- ProjectMVC/Models/Employee.cs
+++
@@ -1,8 +0,0 @@
-namespace ProjectMVC.Models;
-
-public class Employee{
-    public string EmployeeId {get; set;}
-    public string FullName {get; set;}
-    public string Age {get; set;}
-    public string Salary {get; set;}
-}
