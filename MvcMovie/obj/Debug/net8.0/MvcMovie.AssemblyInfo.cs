f8911537c767459fe94d25bcff2218fa790eae26
TruongManhHoang <truongmanhhoang481@gmail.com>
2024-03-01 22:29:20 +0700

    commit

--- MvcMovie/obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c91e31fcfe95bd828b4baf1e429449a9bec418c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8b1cb3e547dab08c858331811cdb4ec0346f537")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
